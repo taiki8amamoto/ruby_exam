34069bc52f7279f391f399bc41e6a61e8149992f
taiki yamamoto <ultra0115.20@gmail.com>
2023-07-10 14:51:49 +0900

    じゃんけんゲーム実装

--- janken.rb
+++ janken.rb
@@ -6,19 +6,24 @@ class Player
     puts "0:グー, 1:チョキ, 2:パー"
     # 変数「input_hand」にプレイヤーの入力値を代入します。
     # ヒント：getsメソッドについて調べてみましょう。
-    input_hand =
+    input_hand = gets.chomp
+    a = ["0", "1", "2"]
     # 「input_hand」が「0, 1, 2」のいずれかだと繰り返し処理を終了し、それ以外（アルファベットも含む）だと繰り返し処理を継続します。
     while true
       # if 「input_hand」が「0, 1, 2」のいずれかの場合だった場合
       # ヒント：include?メソッドについて調べてみましょう。
+      if a.include?(input_hand)
         # 「input_hand」をそのまま返す。
         # ヒント：戻り値を返して繰り返し処理を終了させたい場合、「return」を使用します。
-      # else それ以外の場合
+        return input_hand.to_i
+      else #それ以外の場合
         # プレイヤーに「0〜2」を入力させる文章を表示させる。
-        # puts "0〜2の数字を入力してください。"
-        # puts "0:グー, 1:チョキ, 2:パー"
+        puts "0〜2の数字を入力してください。"
+        puts "0:グー, 1:チョキ, 2:パー"
         # 変数「input_hand」にプレイヤーの入力値を代入します。
+        input_hand = gets.chomp
       # end if文のend
+      end
     end
   end
 end
@@ -26,6 +31,9 @@ end
 class Enemy
   def hand
     # グー、チョキ、パーの値をランダムに取得する。
+    input_hand_arry = [0, 1, 2]
+    input_hand_arry.sample
+
   end
 end
 # プレイヤー(自分)が入力した「0~2」と、敵がランダムで生成した「0~2」をじゃんけんをさせて、その結果をコンソール上に出力するロジックを書きます。
@@ -36,18 +44,25 @@ class Janken
     #「相手の手は#{相手の手}です。」と出力させます。
     puts "相手の手は#{janken[enemy_hand]}です。"
     # Playerクラスの戻り値とEnemyクラスの戻り値からじゃんけんするロジックを作成します。
-    if # Playerクラスの戻り値(player_hand)とEnemyクラスの戻り値(enemy_hand)の値が同じだった場合
+    # Playerクラスの戻り値(player_hand)とEnemyクラスの戻り値(enemy_hand)の値が同じだった場合
+    if player_hand == enemy_hand
       # 「あいこ」を出力します。
       puts "あいこ"
       #「true」を返してじゃんけんを繰り返し実行させます。
       # ヒント：「return」を使って戻り値を返すことができます。しかし、Rubyでは戻り値を返す場合、「return」を省略するのが一般的です。
-    elsif # もしも下記の組み合わせだった場合
+      true
+    # もしも下記の組み合わせだった場合
       # (player_hand == 0 && enemy_hand == 1) || (player_hand == 1 && enemy_hand == 2) || (player_hand == 2 && enemy_hand == 0)
+    elsif (player_hand == 0 && enemy_hand == 1) || (player_hand == 1 && enemy_hand == 2) || (player_hand == 2 && enemy_hand == 0)
       #「あなたの勝ちです」を出力します。
+      puts "あなたの勝ちです"
       #「false」を返してじゃんけんを終了させます。
+      false
     else
       #「あなたの負けです」を出力します。
+      puts "あなたの負けです"
       #「false」を返してじゃんけんを終了させます。
+      false
     end
   end
 end
@@ -62,8 +77,9 @@ class GameStart
     # 変数「janken」にJankenをインスタンス化したものを代入します。
     janken = Janken.new
     # 変数「next_game」に「true」を代入しましょう。
+    next_game = true
     # 「next_game」が「false」だと繰り返し処理を終了し、「true」だと繰り返し処理を継続します。
-    while next_game
+    while next_game == true
       # 変数「next_game」にじゃんけんを実行して返ってきた値(戻り値)を代入します。
       #「janken.pon(player.hand, enemy.hand)」でじゃんけんを実行しています。
       next_game = janken.pon(player.hand, enemy.hand)
